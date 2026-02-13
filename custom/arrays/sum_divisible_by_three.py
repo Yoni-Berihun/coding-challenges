ab8a37fad00044b2ac751cf32d69435aa3a6eb77
Yoni-Berihun <yonatanberihun1998@gmail.com>
2026-02-13 15:57:24 +0300

    added quesiton for sum_divisible_by _three problem

---
+++ custom/arrays/sum_divisible_by_three.py
@@ -0,0 +1,7 @@
+# 📄 Problem Question
+
+# You are given an integer n representing the size of an array and a list of n integers.
+
+# If the sum of the integers is divisible by 3, return the maximum value in the array.
+
+# Otherwise, return the minimum value in the array.
